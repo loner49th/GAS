e85e8d1997ba7ba6bc233c1ce190d12ae6b98377
loner49th <loner49th@gmail.com>
2020-04-05 23:13:33 +0900

    インデントを修正

--- ChangeSpreadSheetCell.js
+++ ChangeSpreadSheetCell.js
@@ -1,17 +1,17 @@
 function myFunction() {
-    var files = DriveApp.getFolderById('セルに値をセットしたいスプレッドシートがあるフォルダ').getFiles();
-    var file;
-  
-  　while(files.hasNext()) {
-     file = files.next();
-     str = file.getName();
-     var ss = SpreadsheetApp.openByUrl(file.getUrl());
-     var sheets = ss.getSheets();
-     var sheet = sheets[0];
-     const lastRow = sheet.getLastRow();
-     const lastColumn = sheet.getLastColumn();
-    for(let i = 1; i <= lastRow; i++) {
-           sheet.getRange(i, 6).setValue(Function(sheet.getRange(i,1).getValue()));
-        }
-    } 
-  }
+	var files = DriveApp.getFolderById('セルに値をセットしたいスプレッドシートがあるフォルダ').getFiles();
+	var file;
+
+	while (files.hasNext()) {
+		file = files.next();
+		str = file.getName();
+		var ss = SpreadsheetApp.openByUrl(file.getUrl());
+		var sheets = ss.getSheets();
+		var sheet = sheets[0];
+		const lastRow = sheet.getLastRow();
+		const lastColumn = sheet.getLastColumn();
+		for (let i = 1; i <= lastRow; i++) {
+			sheet.getRange(i, 6).setValue(Function(sheet.getRange(i, 1).getValue()));
+		}
+	}
+}
